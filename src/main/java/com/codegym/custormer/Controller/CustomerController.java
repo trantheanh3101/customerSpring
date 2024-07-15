ca9cc11110404a4a67ed259ab6565addccea89eb
trantheanh3101 <trantheanhk11@gmail.com>
2024-07-15 14:33:41 +0700

    lt

--- src/main/java/com/codegym/custormer/Controller/CustomerController.java
+++ src/main/java/com/codegym/custormer/Controller/CustomerController.java
@@ -6,16 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("customers")
 public class CustomerController {
 
     @Autowired
     private ICustomerService customerService;
 
-    @GetMapping("/customers")
+    @GetMapping("")
     public String showList(Model model) {
         List<Customer> customers = customerService.findAll();
         model.addAttribute("customers",customers);
